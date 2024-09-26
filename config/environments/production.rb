e2ddb5be263d5a9b0e95a60819d4495ccc5785ee
Mason Roberts <mrobe18@gmail.com>
2024-09-26 15:11:18 -0400

    changed smtp port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:         'smtp.sendgrid.net',
-    port:            465,
+    port:            587,
     user_name:       'apikey',
     password:        Rails.application.credentials.dig(:sendgrid_password),
     authentication:  'plain',
