5f4da1f5bc005bff709e68a440ab796492324981
Mason Roberts <mrobe18@gmail.com>
2024-09-28 20:32:20 -0400

    change env config

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require_relative "application"
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey',
-  password: Rails.application.credentials.dig(:sendgrid, :sendgrid_password),
+  password: Rails.application.credentials.dig(:sendgrid_password),
   domain: 'www.truckerjobs4u.com',
   address: 'smtp.sendgrid.net',
   port: 587,
